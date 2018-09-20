415295ebc2928b5c71e7cd843bca620638a2d2ef
Yomi Toba <ytwguru@gmail.com>
2018-09-20 16:35:21 -0500

    Setup live videos

--- wp-content/mu-plugins/hectv/classes/hectv_live_videos.php
+++ wp-content/mu-plugins/hectv/classes/hectv_live_videos.php
@@ -11,32 +11,15 @@ class HECTV_Live_Videos extends HECTV_Routes  implements HECTV_Custom_Post_Inter
     function __construct($post_type){
 
         $this->post_type = $post_type;
-        $this->param_list = ["start"];
+        $this->param_list = [];
 
         $this->setup_params($post_type, $this->param_list);
         $this->init();
     }
 
-    public function param_start( $args, $request ){
-        $current_time   = date("Y-m-d h:i:s");
-        $meta_query = array(
-            'relation' => 'AND',
-            array(
-                'key'     => 'start_date',
-                'value'   => $current_time,
-                'compare' => '<=',
-                'type'    => 'DATETIME'
-            ),
-            array(
-                'key'     => 'end_date',
-                'value'   => $current_time,
-                'compare' => '>=',
-                'type'    => 'DATETIME'
-            ),
-        );
-        return $this->getNewMetaQuery($args, $meta_query);
-    }
-
+    /**
+     * Callback function registers the event post type
+     */
     public function register_post_type()
     {
         $labels = array(
@@ -48,7 +31,7 @@ class HECTV_Live_Videos extends HECTV_Routes  implements HECTV_Custom_Post_Inter
             'new_item' => __( 'New Live Video' ),
             'view_item' => __( 'View Live Video' ),
             'search_items' => __( 'Search Live Videos' ),
-            'not_found' =>  __( 'No events found' ),
+            'not_found' =>  __( 'No videos found' ),
             'not_found_in_trash' => __( 'No events found in Trash' ),
             'parent_item_colon' => ''
         );
@@ -59,20 +42,21 @@ class HECTV_Live_Videos extends HECTV_Routes  implements HECTV_Custom_Post_Inter
             'publicly_queryable' => false,
             'show_ui' => true,
             'query_var' => true,
-            'capability_type' => 'page',
+            'capability_type' => 'post',
             'menu_icon' => 'dashicons-video-alt3',
             'hierarchical' => false,
-            'supports' => array( 'title', 'editor', 'comments', 'author' ),
+            'supports' => array( 'title', 'editor', 'thumbnail' ),
             'show_in_rest' => true,
-            'menu_position' => 9,
+            'menu_position' => 5,
             'rest_base' => $this->post_type,
         );
+
         register_post_type( $this->post_type, $args );
     }
 
 
     public function init()
     {
-        add_filter( 'init', array( $this, 'register_post_type' ), 0 );
+        add_filter( "init", array( $this, 'register_post_type' ), 0 );
     }
 }
